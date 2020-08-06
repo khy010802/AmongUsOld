ec31b9237c78f0f1445f533034af49026f771d2e
BuildTools <unconfigured@null.spigotmc.org>
2020-08-07 04:30:36 +0900

    # WARNING: head commit changed in the meantime
    ddd

---
+++ src/bepo/au/missions/EmptyChute.java
@@ -0,0 +1,126 @@
+package bepo.au.missions;
+
+
+import java.util.HashSet;
+
+
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+ 
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.ClickType;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryDragEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+
+import bepo.au.Util;
+
+
+
+
+public class EmptyChute implements Listener {
+	 Main main;
+	 Player p;
+	 Inventory gui;
+	 int maxLeaves=5;  //해야하는 나뭇잎 개수 HARD EASY 조절 가능. 최대 36개.
+	 int remainingLeaves=maxLeaves;
+	 int maxslot=45;
+	 
+	public  void emptychute(Player pl, Main m) {
+		main=m;
+		p=pl;
+		
+		reset();
+	}
+	public  void reset() {
+		gui = Bukkit.createInventory(p, maxslot, "EmptyChute");
+		gui.setMaxStackSize(1);
+		int index=0;
+		HashSet<Integer> leafSlots = new HashSet<Integer>();
+		while (true) {
+			int leafslot = Util.random(3,8)+9*Util.random(0,4);
+			Util.debugMessage(leafslot+"에 나뭇잎");
+			leafSlots.add(leafslot);
+			if(leafSlots.size()==maxLeaves) break;
+			}
+			
+			for(int slot = 0 ; slot<maxslot; slot++) {
+				int x=slot%9 ,y=slot/9;
+				if (x<3) { 
+					if(slot==20) Util.Stack(gui, slot, Material.HOPPER, 1, "§f이곳에 나뭇잎을 들고 클릭해주세요");
+					else if (slot==18) Util.Stack(gui, slot, Material.BOOK, 1, "§f산소 필터 청소하기", "§7나뭇잎을 들고 호퍼에 클릭하세요.");
+					else if (x>0&&y>0&&y<=3) Util.Stack(gui, slot, Material.WHITE_STAINED_GLASS_PANE, 1, " ");
+					else  Util.Stack(gui, slot, Material.GRAY_STAINED_GLASS_PANE, 1, " ");
+				}
+				else if (leafSlots.contains(slot)) {
+					
+					Util.Stack(gui, slot, Material.KELP, 1, "§f나뭇잎", "§7이것을 든 채로 호퍼를 클릭하세요.",(index++));
+				}
+		}
+		p.openInventory(gui);
+	}
+
+	public void removeLeaf() {
+		if (--remainingLeaves==0) {
+			Util.debugMessage("클리어!");
+			return;
+			}
+		Util.debugMessage(remainingLeaves+"개 남음");
+		
+	}
+	
+	
+	
+	
+
+@EventHandler
+public void onClick(InventoryClickEvent e) {
+	if(e.getView().getTitle().equals("EmptyChute")) {
+	Util.debugMessage("클릭 인식됨");
+	//int slot = e.getRawSlot();
+	ItemStack itemstack = e.getCurrentItem();
+	
+	//Inventory gui = e.getClickedInventory();
+	//Player p = (Player) e.getWhoClicked();
+	
+	
+		if(e.getClick().equals(ClickType.DOUBLE_CLICK) || e.isShiftClick() == true) { //더블클릭,쉬프트클릭 금지
+			Util.debugMessage("클릭 불가");
+			e.setCancelled(true);
+		}
+		if(itemstack!=null) {
+			if(e.getCursor().getType()==Material.AIR&&itemstack.getType()==Material.KELP) Util.debugMessage("켈프 클릭 인식됨");
+			else if(e.getCursor().getType()==Material.KELP&&itemstack.getType()==Material.HOPPER) {
+				removeLeaf();
+				e.getWhoClicked().setItemOnCursor(null);
+				e.setCancelled(true);
+			}
+			else {
+				Util.debugMessage("클릭 불가");
+				e.setCancelled(true);
+				}
+			}
+	}	
+	}
+	
+
+	@EventHandler
+public void onDrag(InventoryDragEvent e) {
+	if (e.getView().getTitle().equals("EmptyChute")&&!e.getRawSlots().isEmpty()){
+	;
+	}
+	
+
+}
+
+
+
+
+
+}
+	
+	
+	
