29354549c2316001edb4687813dbab6ccdf728d7
machamy <macham2001@gmail.com>
2020-09-15 21:38:29 +0900

    gui 모양 수정

--- src/bepo/au/AdminMap.java
+++ src/bepo/au/AdminMap.java
@@ -18,23 +18,23 @@ import net.minecraft.server.v1_16_R2.SlotShulkerBox;
 @SuppressWarnings("unused")
 public class AdminMap implements Listener {
 	private HashMap<String, Inventory> guiMap;
-	private final int maxslot = 45;
+	private final int maxslot = 54;
 	public final String guiName = "관리실 지도";
 	private enum ROOMS{
 		CAFE(4,"식당",Material.ENCHANTING_TABLE, Material.RED_WOOL),
-		UE(10,"상부 엔진",Material.DISPENSER, Material.BLUE_WOOL),
+		UE(1,"상부 엔진",Material.DISPENSER, Material.BLUE_WOOL),
 		MEDBEY(12,"의무실",Material.POTION, Material.LIME_WOOL),
-		O2(14,"산소 공급실",Material.KELP, Material.GREEN_WOOL),
-		WEAPONS(15,"무기고",Material.FIREWORK_ROCKET, Material.BROWN_WOOL),
+		O2(15,"산소 공급실",Material.KELP, Material.GREEN_WOOL),
+		WEAPONS(7,"무기고",Material.FIREWORK_ROCKET, Material.BROWN_WOOL),
 		REACTOR(18,"원자로",Material.CRYING_OBSIDIAN, Material.BLACK_WOOL),
 		SECU(20,"보안실",Material.ENDER_EYE, Material.MAGENTA_WOOL),
 		ADMIN(23,"관리실",Material.MAP, Material.PURPLE_WOOL),
-		NAVI(25,"항해실",Material.LODESTONE, Material.GRAY_WOOL),
-		LE(28,"하부엔진",Material.DISPENSER, Material.LIGHT_BLUE_WOOL),
+		NAVI(26,"항해실",Material.LODESTONE, Material.GRAY_WOOL),
+		LE(37,"하부엔진",Material.DISPENSER, Material.LIGHT_BLUE_WOOL),
 		ELEC(30,"전기실",Material.COMPARATOR, Material.YELLOW_WOOL),
-		SHIELD(33,"보호막 제어실",Material.BEACON, Material.CYAN_WOOL),
+		SHIELD(43,"보호막 제어실",Material.BEACON, Material.CYAN_WOOL),
 		STORAGE(40,"창고",Material.CHEST, Material.ORANGE_WOOL),
-		COMM(41,"통신실",Material.SOUL_TORCH, Material.WHITE_WOOL);
+		COMM(51,"통신실",Material.SOUL_TORCH, Material.WHITE_WOOL);
 		
 		
 		private final int slot;
@@ -85,9 +85,10 @@ public class AdminMap implements Listener {
 				ROOMS room = ROOMS.getROOM(slot);
 				Util.Stack(guiMap.get(name), slot, room.getMat(), 1, "§f"+room.getRoomname(),"§7현재인원 : 0");
 			}
-			else {
-					Util.Stack(guiMap.get(name), slot, Material.GREEN_STAINED_GLASS_PANE, 1, " ");
+			else if((slot>0&&slot<8)||(slot>36&&slot<44)||slot%9==1||slot%9==4||slot%9==7){
+					Util.Stack(guiMap.get(name), slot, Material.GREEN_STAINED_GLASS_PANE, 1, "§7통로");
 			}
+			
 		}
 	}
 	private void updateRoom(Player p, ROOMS room, int amount) {
